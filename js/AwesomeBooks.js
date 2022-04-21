0a1fc24bcf11a2599d2e1d9c76f490ce3df61e03
gids-dadi <gideons564@gmail.com>
2022-04-21 09:53:47 +0100

    Modify to use classes

---
+++ js/AwesomeBooks.js
@@ -0,0 +1,44 @@
+
+class AwesomeBooks {
+ constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+    this.books = [];
+  }
+
+  forBook(title, author) {
+    const id = this.books.length + 1;
+    const book = new AwesomeBooks(title, author, id);
+    this.books.push(book);
+    this.#save();
+  }
+
+  #ForBooks(books) {
+    books.forEach((book) => {
+      this.forBook(book.title, book.author);
+    });
+  }
+
+  getBooks() {
+    return this.books;
+  }
+
+  #save() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  load() {
+    const books = JSON.parse(localStorage.getItem('books'));
+    if (books) {
+      this.#ForBooks(books);
+    }
+  }
+
+  deleteBook(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+    this.#save();
+  }
+}
+
+export default AwesomeBooks;
---
+++ js/main.js
@@ -0,0 +1,42 @@
+import AwesomeBooks from './AwesomeBooks.js';
+
+const awesomeBooks = new AwesomeBooks();
+awesomeBooks.load();
+if (awesomeBooks.getBooks().length < 1) {
+  awesomeBooks.addBook('The Hobbit', 'J.R.R. Tolkien');
+  awesomeBooks.addBook('The Lord of the Rings', 'J.R.R. Tolkien');
+  awesomeBooks.addBook('The Catcher in the Rye', 'J.D. Salinger');
+  awesomeBooks.addBook('The Grapes of Wrath', 'John Steinbeck');
+}
+
+const bookList = document.querySelector('#bookList');
+
+function renderBookList() {
+  bookList.innerHTML = awesomeBooks.getBooks().map((book, index) => `
+        <article class="book ${index % 2 === 0 ? 'dark' : ''}">
+            <div>
+                <p class="title">"${book.title}" by ${book.author}</p>
+            </div>
+            <button data-id=${book.id} class="remove">Remove</button>
+        </article>`).join('');
+}
+
+renderBookList();
+
+const addBookForm = document.querySelector('#bookEntry');
+addBookForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const title = event.target.querySelector('#bookTitle').value;
+  const author = event.target.querySelector('#bookAuthor').value;
+  awesomeBooks.forBook(title, author);
+  this.reset();
+  renderBookList();
+});
+
+bookList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove')) {
+    const { id } = event.target.dataset;
+    awesomeBooks.deleteBook(+id);
+    renderBookList();
+  }
+});
